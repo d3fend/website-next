e54f89c71fa25f235a0d02383ef71c4ce64a6b52
Peter Kaloroumakis <pk@mitre.org>
2022-05-18 21:09:06 -0400

    init

---
+++ service-worker.js
@@ -0,0 +1,164 @@
+const build = [
+  "/_app/start-9eaa1704.js",
+  "/_app/pages/__layout.svelte-bde4ab7a.js",
+  "/_app/assets/pages/__layout-matrix.svelte-5f711740.css",
+  "/_app/pages/__error.svelte-1d825c42.js",
+  "/_app/pages/__layout-matrix.svelte-d3a64646.js",
+  "/_app/pages/mappings/__layout.svelte-9c60b20b.js",
+  "/_app/pages/offensive-technique/attack/__layout@matrix.svelte-7861c5d0.js",
+  "/_app/pages/progress/__layout.svelte-5e5c52fc.js",
+  "/_app/pages/tactic/__layout-tactic@matrix.svelte-fa82d9fc.js",
+  "/_app/assets/pages/tactic/__layout-tactic@matrix.svelte-b7a77fc7.css",
+  "/_app/pages/technique/__layout-tech@matrix.svelte-0898f6f3.js",
+  "/_app/pages/about.svelte-5bf95ec1.js",
+  "/_app/assets/pages/about.svelte-b0b5b64a.css",
+  "/_app/pages/contact.svelte-27af9ef7.js",
+  "/_app/assets/Button.svelte_svelte_type_style_lang-7ef90c91.css",
+  "/_app/pages/contribute/index.svelte-398ce257.js",
+  "/_app/pages/dao/artifact/_artifact_.svelte-3fd4e383.js",
+  "/_app/assets/pages/dao/artifact/_artifact_.svelte-76d0c295.css",
+  "/_app/assets/SimpleAutocomplete.svelte_svelte_type_style_lang-c01bf9e8.css",
+  "/_app/pages/dao/artifact/index.svelte-8942ab3d.js",
+  "/_app/pages/dao/index.svelte-8839d9d2.js",
+  "/_app/assets/pages/dao/index.svelte-43d7df8e.css",
+  "/_app/pages/faq/index.svelte-cfbc359d.js",
+  "/_app/pages/index@matrix.svelte-4af1c451.js",
+  "/_app/assets/pages/index@matrix.svelte-3b40261d.css",
+  "/_app/pages/mappings/attack-mitigations.svelte-c5ac8ae0.js",
+  "/_app/pages/mappings/index.svelte-1c5de77e.js",
+  "/_app/pages/offensive-technique/attack/_technique_.svelte-f8d96d57.js",
+  "/_app/assets/pages/offensive-technique/attack/_technique_.svelte-b6dc9aa9.css",
+  "/_app/pages/offensive-technique/index.svelte-b0703d6c.js",
+  "/_app/pages/ontology/describe/_slug_.svelte-0393b459.js",
+  "/_app/assets/pages/ontology/describe/_slug_.svelte-55af8b4a.css",
+  "/_app/pages/poster@matrix.svelte-ae70ded0.js",
+  "/_app/assets/pages/poster@matrix.svelte-ad010473.css",
+  "/_app/pages/privacy.svelte-c80c93be.js",
+  "/_app/pages/progress/index.svelte-e7c335fc.js",
+  "/_app/pages/resources/index.svelte-9fa12dee.js",
+  "/_app/pages/resources/ontology.svelte-2dd31f08.js",
+  "/_app/pages/search.svelte-487768bc.js",
+  "/_app/assets/pages/search.svelte-6a857f2b.css",
+  "/_app/pages/tactic/_tactic_@tactic.svelte-5cf38ee0.js",
+  "/_app/assets/pages/tactic/_tactic_@tactic.svelte-72e063e4.css",
+  "/_app/pages/tactic/index.svelte-7b9f2696.js",
+  "/_app/pages/technique/_technique_@tech.svelte-263f8c5f.js",
+  "/_app/assets/pages/technique/_technique_@tech.svelte-4c576568.css",
+  "/_app/pages/technique/index.svelte-09e4c364.js",
+  "/_app/pages/tools/attack-extractor.svelte-6ac101df.js",
+  "/_app/assets/pages/tools/attack-extractor.svelte-81c1f600.css",
+  "/_app/pages/tools/d3fend-extractor.svelte-b183e823.js",
+  "/_app/assets/pages/tools/d3fend-extractor.svelte-120a3a9b.css",
+  "/_app/pages/tools/index.svelte-75caa666.js",
+  "/_app/pages/tou.svelte-4cff5701.js",
+  "/_app/chunks/index-a02da30e.js",
+  "/_app/chunks/index-98a102c4.js",
+  "/_app/chunks/singletons-d1fb5791.js",
+  "/_app/chunks/Nav-mobile-db0c9fed.js",
+  "/_app/assets/Nav-mobile-e0f80dc0.css",
+  "/_app/chunks/stores-59d8ae96.js",
+  "/_app/chunks/stores-cf129ad8.js",
+  "/_app/chunks/config-6e32f34b.js",
+  "/_app/chunks/lib-c1de3c87.js",
+  "/_app/chunks/marked.esm-8ab30116.js",
+  "/_app/chunks/D3FMatrix-3aa45275.js",
+  "/_app/assets/D3FMatrix-6787df72.css",
+  "/_app/chunks/navigation-0e6511d1.js",
+  "/_app/chunks/Header-826dac44.js",
+  "/_app/assets/Header-6acab121.css",
+  "/_app/chunks/Title-1d23c88f.js",
+  "/_app/chunks/Button-db445531.js",
+  "/_app/chunks/BlockTree-492566e1.js",
+  "/_app/assets/BlockTree-54ffff92.css",
+  "/_app/chunks/Alert-0c17ca36.js",
+  "/_app/assets/Alert-f8df0c1c.css",
+  "/_app/chunks/Lookup-DAO-0ce5045d.js",
+  "/_app/assets/Lookup-DAO-2347e66b.css",
+  "/_app/chunks/mermaid.esm.min-4bfe481d.js",
+  "/_app/chunks/Modal_full-0e5fb963.js",
+  "/_app/assets/Modal_full-83ebf193.css",
+  "/_app/chunks/Table-79382ad4.js",
+  "/_app/chunks/TechniqueSubclasses-efbe15c1.js",
+  "/_app/assets/TechniqueSubclasses-62da13f8.css"
+];
+const files = [
+  "/favicon.ico",
+  "/font/azonix/azonix-d3fend.otf",
+  "/global-d3fend.css",
+  "/img/attack-dao-defend.png",
+  "/img/attack-defend.png",
+  "/img/attack-mapper.png",
+  "/img/csd.png",
+  "/img/csv.png",
+  "/img/d3fend-og.png",
+  "/img/d3fend-poster-thumb.png",
+  "/img/graph.png",
+  "/img/graph1.png",
+  "/img/json.png",
+  "/img/mappings-mitigations.png",
+  "/img/mitre.png",
+  "/img/nsa.png",
+  "/img/owl.png",
+  "/img/paper.png",
+  "/img/ttl.png",
+  "/ontologies/.git",
+  "/ontologies/README.md",
+  "/ontologies/d3fend/0.9.3-BETA-1/d3fend.json",
+  "/ontologies/d3fend/0.9.3-BETA-1/d3fend.owl",
+  "/ontologies/d3fend/0.9.3-BETA-1/d3fend.ttl",
+  "/ontologies/d3fend.csv",
+  "/ontologies/d3fend.json",
+  "/ontologies/d3fend.owl",
+  "/ontologies/d3fend.ttl",
+  "/resources/D3FEND.pdf",
+  "/resources/d3fend-full-mappings.csv",
+  "/resources/d3fend-full-mappings.json",
+  "/resources/extra-dist/CNAME",
+  "/resources/extra-dist/README.md"
+];
+const version = "1652922064716";
+const ASSETS = `cache${version}`;
+const to_cache = build.concat(files);
+const staticAssets = new Set(to_cache);
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(ASSETS).then((cache) => cache.addAll(to_cache)).then(() => {
+    self.skipWaiting();
+  }));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then(async (keys) => {
+    for (const key of keys) {
+      if (key !== ASSETS)
+        await caches.delete(key);
+    }
+    self.clients.claim();
+  }));
+});
+async function fetchAndCache(request) {
+  const cache = await caches.open(`offline${version}`);
+  try {
+    const response = await fetch(request);
+    cache.put(request, response.clone());
+    return response;
+  } catch (err) {
+    const response = await cache.match(request);
+    if (response)
+      return response;
+    throw err;
+  }
+}
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET" || event.request.headers.has("range"))
+    return;
+  const url = new URL(event.request.url);
+  const isHttp = url.protocol.startsWith("http");
+  const isDevServerRequest = url.hostname === self.location.hostname && url.port !== self.location.port;
+  const isStaticAsset = url.host === self.location.host && staticAssets.has(url.pathname);
+  const skipBecauseUncached = event.request.cache === "only-if-cached" && !isStaticAsset;
+  if (isHttp && !isDevServerRequest && !skipBecauseUncached) {
+    event.respondWith((async () => {
+      const cachedAsset = isStaticAsset && await caches.match(event.request);
+      return cachedAsset || fetchAndCache(event.request);
+    })());
+  }
+});
