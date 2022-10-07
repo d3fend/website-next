c758f31cdb5b54137568e7baf34bb064092aa71c
Peter Kaloroumakis <pk@mitre.org>
2022-10-07 15:50:00 -0600

    automatic deployment to staging site

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
 const build = [
-  "/_app/start-bf00bd0c.js",
+  "/_app/start-01644ecd.js",
   "/_app/pages/__layout.svelte-f1c91157.js",
-  "/_app/assets/pages/__layout.svelte-a0ff9840.css",
+  "/_app/assets/pages/__layout-matrix.svelte-5f711740.css",
   "/_app/pages/__error.svelte-1d825c42.js",
   "/_app/pages/__layout-matrix.svelte-dace5147.js",
   "/_app/pages/mappings/__layout.svelte-9c60b20b.js",
-  "/_app/pages/offensive-technique/attack/__layout@matrix.svelte-334cf648.js",
+  "/_app/pages/offensive-technique/attack/__layout@matrix.svelte-1b749f57.js",
   "/_app/pages/progress/__layout.svelte-5e5c52fc.js",
-  "/_app/pages/tactic/__layout-tactic@matrix.svelte-197286a8.js",
-  "/_app/assets/pages/tactic/__layout-tactic@matrix.svelte-b7a77fc7.css",
-  "/_app/pages/technique/__layout-tech@matrix.svelte-b22eab37.js",
+  "/_app/pages/tactic/__layout-tactic@matrix.svelte-4cb76931.js",
+  "/_app/assets/pages/technique/__layout-tech@matrix.svelte-c1854619.css",
+  "/_app/pages/technique/__layout-tech@matrix.svelte-b94ebfe5.js",
   "/_app/pages/about.svelte-5bf95ec1.js",
   "/_app/assets/pages/about.svelte-b0b5b64a.css",
   "/_app/pages/contact.svelte-27af9ef7.js",
@@ -23,7 +23,7 @@ const build = [
   "/_app/pages/dao/index.svelte-06cfaca7.js",
   "/_app/assets/pages/dao/index.svelte-43d7df8e.css",
   "/_app/pages/faq/index.svelte-cfbc359d.js",
-  "/_app/pages/index@matrix.svelte-e499ae80.js",
+  "/_app/pages/index@matrix.svelte-2bee6553.js",
   "/_app/assets/pages/index@matrix.svelte-3b40261d.css",
   "/_app/pages/mappings/attack-mitigations.svelte-c3b092e4.js",
   "/_app/pages/mappings/index.svelte-1c5de77e.js",
@@ -33,7 +33,7 @@ const build = [
   "/_app/pages/offensive-technique/index.svelte-cd83ad1b.js",
   "/_app/pages/ontology/describe/_slug_.svelte-38d4dea9.js",
   "/_app/assets/pages/ontology/describe/_slug_.svelte-55af8b4a.css",
-  "/_app/pages/poster@matrix.svelte-cc767844.js",
+  "/_app/pages/poster@matrix.svelte-0c63fc06.js",
   "/_app/assets/pages/poster@matrix.svelte-ad010473.css",
   "/_app/pages/privacy.svelte-ffcf299b.js",
   "/_app/pages/progress/index.svelte-e1741567.js",
@@ -41,10 +41,10 @@ const build = [
   "/_app/pages/resources/ontology.svelte-54817cb5.js",
   "/_app/pages/search.svelte-2731381b.js",
   "/_app/assets/pages/search.svelte-6a857f2b.css",
-  "/_app/pages/tactic/_tactic_@tactic.svelte-69fd2a0a.js",
+  "/_app/pages/tactic/_tactic_@tactic.svelte-a462d220.js",
   "/_app/assets/pages/tactic/_tactic_@tactic.svelte-72e063e4.css",
   "/_app/pages/tactic/index.svelte-c888c3ba.js",
-  "/_app/pages/technique/_technique_@tech.svelte-c26de0c4.js",
+  "/_app/pages/technique/_technique_@tech.svelte-0e2c7d62.js",
   "/_app/assets/pages/technique/_technique_@tech.svelte-4c576568.css",
   "/_app/pages/technique/index.svelte-757e29e9.js",
   "/_app/pages/tools/artifact-extractor.svelte-299cb5bd.js",
@@ -65,7 +65,7 @@ const build = [
   "/_app/chunks/config-9d8d9fe3.js",
   "/_app/chunks/lib-cf80d2a1.js",
   "/_app/chunks/marked.esm-8ab30116.js",
-  "/_app/chunks/D3FMatrix-2a7a9607.js",
+  "/_app/chunks/D3FMatrix-1a8a18e3.js",
   "/_app/assets/D3FMatrix-2034dabc.css",
   "/_app/chunks/navigation-0e6511d1.js",
   "/_app/chunks/Header-47fd749a.js",
@@ -81,7 +81,7 @@ const build = [
   "/_app/chunks/Modal_full-a93e1e32.js",
   "/_app/assets/Modal_full-621476ac.css",
   "/_app/chunks/Table-79382ad4.js",
-  "/_app/chunks/TechniqueSubclasses-efbe15c1.js",
+  "/_app/chunks/TechniqueSubclasses-e8b03f71.js",
   "/_app/assets/TechniqueSubclasses-62da13f8.css",
   "/_app/chunks/extract-e577679c.js",
   "/_app/assets/extract-d25bbc7d.css"
@@ -125,7 +125,7 @@ const files = [
   "/resources/extra-dist/CNAME",
   "/resources/extra-dist/README.md"
 ];
-const version = "1664570348234";
+const version = "1665178770035";
 const ASSETS = `cache${version}`;
 const to_cache = build.concat(files);
 const staticAssets = new Set(to_cache);
