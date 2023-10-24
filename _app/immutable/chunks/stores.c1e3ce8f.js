d5c0a4a02d3b89b7da2c61a62b9390e0efbb3c3e
Peter Kaloroumakis <pk@mitre.org>
2023-10-24 14:42:14 -0600

    automatic deployment to staging site

--- _app/immutable/chunks/stores.c1e3ce8f.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./singletons.5ce3a91f.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
