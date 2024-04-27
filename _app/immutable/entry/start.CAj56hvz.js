1e794ddaa405d35aa00d709fc154da87e31797b3
Peter Kaloroumakis <pk@mitre.org>
2024-04-26 22:00:04 -0600

    automatic deployment to staging site

---
+++ _app/immutable/entry/start.CAj56hvz.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/entry.C0gnFZlW.js";export{a as start};
---
+++ _app/immutable/nodes/38.BOl4KRpg.js
@@ -0,0 +1,104 @@
+import{s as Ze,N as Te,e as i,b as E,t as ie,d as r,v as le,j as w,f as b,i as c,h as re,w as a,k as ve,l as t,p as me,a4 as xe,X as Ue,m as qe,y as et,o as Je,r as _e,L as lt,C as Qe,q as ot}from"../chunks/scheduler.Cph8Trd5.js";import{S as tt,i as nt,f as Ve,c as De,b as Ie,m as Ne,t as Pe,a as ke,d as $e}from"../chunks/index.CThJCPP4.js";import{p as it}from"../chunks/stores.BlG8hHvn.js";import{T as rt}from"../chunks/Title.B0Sa5AEO.js";import{b as ct}from"../chunks/entry.C0gnFZlW.js";import{S as We}from"../chunks/SimpleAutocomplete.H2Dnecp-.js";import{l as oe}from"../chunks/layers.Bv9rf-Lc.js";import{t as dt}from"../chunks/updateDefendTree.BepZFoY9.js";/* empty css                             */import{J as ut,L as mt,s as ft}from"../chunks/dialog.BdKcBNGP.js";function pt(e){var Ge;let f,m="Settings",n,d,g,O="Profile",S,p,U,h,N,G="Matrix Overlay",F,C,V,W,_,T,k,I,o,y,s,L="Matrix Readonly",H,z,$,A,ee,fe,M,be="Show Side Capabilities",ce,Z,P,q,de,j,R,B="Show Legend",pe,Y,te,Ce,l,v,K=(e[3].containerSize*100).toFixed(0)+"",Q,ge,Ee,J,ne,ae="60",we,x,Se,se,He="100",ue,ze,je;function at(u){e[10](u)}let Be={inputClassName:"autocomplete-cursor-select",items:(Ge=e[3])==null?void 0:Ge.profiles,readonly:!0,labelFieldName:"name",onChange:e[9]};e[2]!==void 0&&(Be.selectedItem=e[2]),p=new We({props:Be}),Te.push(()=>Ve(p,"selectedItem",at));function st(u){e[12](u)}let Xe={inputClassName:"autocomplete-cursor-select",items:Object.values(e[4]),labelFieldName:"name",readonly:!0,onChange:e[11]};return e[1]!==void 0&&(Xe.selectedItem=e[1]),C=new We({props:Xe}),Te.push(()=>Ve(C,"selectedItem",st)),{c(){f=i("h3"),f.textContent=m,n=E(),d=i("article"),g=i("div"),g.textContent=O,S=E(),De(p.$$.fragment),h=E(),N=i("div"),N.textContent=G,F=E(),De(C.$$.fragment),W=E(),_=i("section"),T=i("article"),k=i("form"),I=i("input"),y=E(),s=i("span"),s.textContent=L,H=E(),z=i("article"),$=i("form"),A=i("input"),fe=E(),M=i("span"),M.textContent=be,ce=E(),Z=i("article"),P=i("form"),q=i("input"),j=E(),R=i("span"),R.textContent=B,pe=E(),Y=i("article"),te=i("div"),Ce=ie("Container Scale"),l=i("span"),v=ie("("),Q=ie(K),ge=ie(")"),Ee=E(),J=i("div"),ne=i("span"),ne.textContent=ae,we=E(),x=i("input"),Se=E(),se=i("span"),se.textContent=He,this.h()},l(u){f=r(u,"H3",{class:!0,"data-svelte-h":!0}),le(f)!=="svelte-ihiao5"&&(f.textContent=m),n=w(u),d=r(u,"ARTICLE",{class:!0});var D=b(d);g=r(D,"DIV",{class:!0,"data-svelte-h":!0}),le(g)!=="svelte-nczzpi"&&(g.textContent=O),S=w(D),Ie(p.$$.fragment,D),h=w(D),N=r(D,"DIV",{class:!0,"data-svelte-h":!0}),le(N)!=="svelte-czypmm"&&(N.textContent=G),F=w(D),Ie(C.$$.fragment,D),D.forEach(c),W=w(u),_=r(u,"SECTION",{class:!0});var X=b(_);T=r(X,"ARTICLE",{class:!0});var Le=b(T);k=r(Le,"FORM",{class:!0});var he=b(k);I=r(he,"INPUT",{class:!0,type:!0,name:!0,tabindex:!0}),y=w(he),s=r(he,"SPAN",{class:!0,"data-svelte-h":!0}),le(s)!=="svelte-1saz4fh"&&(s.textContent=L),he.forEach(c),Le.forEach(c),H=w(X),z=r(X,"ARTICLE",{class:!0});var Ye=b(z);$=r(Ye,"FORM",{class:!0});var Me=b($);A=r(Me,"INPUT",{class:!0,type:!0,name:!0,tabindex:!0}),fe=w(Me),M=r(Me,"SPAN",{class:!0,"data-svelte-h":!0}),le(M)!=="svelte-lt3qpu"&&(M.textContent=be),Me.forEach(c),Ye.forEach(c),ce=w(X),Z=r(X,"ARTICLE",{class:!0});var Ke=b(Z);P=r(Ke,"FORM",{class:!0});var Oe=b(P);q=r(Oe,"INPUT",{class:!0,type:!0,name:!0,tabindex:!0}),j=w(Oe),R=r(Oe,"SPAN",{class:!0,"data-svelte-h":!0}),le(R)!=="svelte-1kzsxpu"&&(R.textContent=B),Oe.forEach(c),Ke.forEach(c),pe=w(X),Y=r(X,"ARTICLE",{class:!0});var Fe=b(Y);te=r(Fe,"DIV",{class:!0});var Ae=b(te);Ce=re(Ae,"Container Scale"),l=r(Ae,"SPAN",{});var Re=b(l);v=re(Re,"("),Q=re(Re,K),ge=re(Re,")"),Re.forEach(c),Ae.forEach(c),Ee=w(Fe),J=r(Fe,"DIV",{class:!0});var ye=b(J);ne=r(ye,"SPAN",{"data-svelte-h":!0}),le(ne)!=="svelte-nt3n7g"&&(ne.textContent=ae),we=w(ye),x=r(ye,"INPUT",{type:!0,min:!0,max:!0,class:!0,id:!0}),Se=w(ye),se=r(ye,"SPAN",{"data-svelte-h":!0}),le(se)!=="svelte-rbuf0z"&&(se.textContent=He),ye.forEach(c),Fe.forEach(c),X.forEach(c),this.h()},h(){a(f,"class","text-center"),a(g,"class","select-name svelte-1d9uoe9"),a(N,"class","select-name svelte-1d9uoe9"),a(d,"class","select-setting svelte-1d9uoe9"),a(I,"class","check-box"),a(I,"type","checkbox"),a(I,"name","readonly"),I.checked=o=e[3].isReadonly,a(I,"tabindex","0"),a(s,"class","layer-text"),a(k,"class","svelte-1d9uoe9"),a(T,"class","setting"),a(A,"class","check-box"),a(A,"type","checkbox"),a(A,"name","drawer open"),A.checked=ee=e[3].isDrawerOpen,a(A,"tabindex","0"),a(M,"class","layer-text"),a($,"class","svelte-1d9uoe9"),a(z,"class","setting"),a(q,"class","check-box"),a(q,"type","checkbox"),a(q,"name","show legend"),q.checked=de=e[3].showLegend,a(q,"tabindex","0"),a(R,"class","layer-text"),a(P,"class","svelte-1d9uoe9"),a(Z,"class","setting"),a(te,"class","select-name svelte-1d9uoe9"),a(x,"type","range"),a(x,"min","60"),a(x,"max","99"),x.value="80",a(x,"class","slider"),a(x,"id","myRange"),a(J,"class","size-slider svelte-1d9uoe9"),a(Y,"class","setting"),a(_,"class","setting-checks-container svelte-1d9uoe9")},m(u,D){ve(u,f,D),ve(u,n,D),ve(u,d,D),t(d,g),t(d,S),Ne(p,d,null),t(d,h),t(d,N),t(d,F),Ne(C,d,null),ve(u,W,D),ve(u,_,D),t(_,T),t(T,k),t(k,I),t(k,y),t(k,s),t(_,H),t(_,z),t(z,$),t($,A),t($,fe),t($,M),t(_,ce),t(_,Z),t(Z,P),t(P,q),t(P,j),t(P,R),t(_,pe),t(_,Y),t(Y,te),t(te,Ce),t(te,l),t(l,v),t(l,Q),t(l,ge),t(Y,Ee),t(Y,J),t(J,ne),t(J,we),t(J,x),e[16](x),t(J,Se),t(J,se),ue=!0,ze||(je=[me(I,"click",xe(e[13])),me(A,"click",xe(e[14])),me(q,"click",xe(e[15])),me(x,"input",e[8])],ze=!0)},p(u,[D]){var he;const X={};D&8&&(X.items=(he=u[3])==null?void 0:he.profiles),!U&&D&4&&(U=!0,X.selectedItem=u[2],Ue(()=>U=!1)),p.$set(X);const Le={};!V&&D&2&&(V=!0,Le.selectedItem=u[1],Ue(()=>V=!1)),C.$set(Le),(!ue||D&8&&o!==(o=u[3].isReadonly))&&(I.checked=o),(!ue||D&8&&ee!==(ee=u[3].isDrawerOpen))&&(A.checked=ee),(!ue||D&8&&de!==(de=u[3].showLegend))&&(q.checked=de),(!ue||D&8)&&K!==(K=(u[3].containerSize*100).toFixed(0)+"")&&qe(Q,K)},i(u){ue||(Pe(p.$$.fragment,u),Pe(C.$$.fragment,u),ue=!0)},o(u){ke(p.$$.fragment,u),ke(C.$$.fragment,u),ue=!1},d(u){u&&(c(f),c(n),c(d),c(W),c(_)),$e(p),$e(C),e[16](null),ze=!1,et(je)}}}function gt(e,f,m){var I;let n;Je(e,oe,o=>m(3,n=o));let d=null;const g={multiColor:{name:"multiColor",showLegend:!0},heatMap:{name:"heatMap",showLegend:!1}};let O=g[n==null?void 0:n.techniqueMappingMode]||null,S=((I=n==null?void 0:n.profiles)==null?void 0:I[n==null?void 0:n.selectedProfile])||null;const p=o=>{_e(oe,n[o]=!n[o],n)},U=o=>{var y;if(o){const s=n.profiles.findIndex(({id:L})=>L===o.id);_e(oe,n.selectedProfile=s,n),m(2,S=((y=n==null?void 0:n.profiles)==null?void 0:y[s])||null),dt.reset()}else _e(oe,n.selectedProfile=null,n),m(2,S=o)},h=o=>{if(typeof o=="string")m(1,O=o);else if(o&&o.name){const{name:y="none",showLegend:s}=o;m(1,O=o),_e(oe,n.techniqueMappingMode=y,n),_e(oe,n.showLegend=s,n)}},N=o=>{_e(oe,n.containerSize=o.target.value/100,n)},G=o=>U(o);function F(o){S=o,m(2,S)}const C=o=>h(o);function V(o){O=o,m(1,O)}const W=()=>p("isReadonly"),_=()=>p("isDrawerOpen"),T=()=>p("showLegend");function k(o){Te[o?"unshift":"push"](()=>{d=o,m(0,d)})}return[d,O,S,n,g,p,U,h,N,G,F,C,V,W,_,T,k]}class ht extends tt{constructor(f){super(),nt(this,f,gt,pt,Ze,{})}}const _t=({size:e,domain:f},m="<embed-data>")=>`
+<script>
+  let iframe = null;
+
+  const user_data = ${JSON.stringify(m,null,2)};
+
+  const post_layers = (event) => {
+    if (event.data?.call === "FromParent") {
+      iframe.contentWindow.postMessage(
+        {
+          call: "toParent",
+          value: user_data,
+        },
+        event.origin
+      );
+    }
+  };
+  <\/script>
+
+  <svelte:window on:message={post_layers} />
+
+  <main>
+    <iframe
+      id="iframe"
+      bind:this={iframe}
+      src="${f}/profile/"
+      title="D3FEND Embedded" />
+  </main>
+
+  <style>
+    #iframe {
+      display: block;
+      margin: 0 auto;
+      width: 100%;
+      height: 100vh;
+      transform: scale(${e});
+      -moz-transform: scale(${e});
+      -o-transform: scale(${e});
+      -webkit-transform: scale(${e});
+    }
+  </style>
+  `,vt=({size:e,domain:f},m="<embed-data>")=>`
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>D3FEND EMBEDDED</title>
+
+  <script>
+    const user_data = ${JSON.stringify(m,null,2)};
+
+    const post_layers = (event) => {
+      const iframe = document.getElementById("iframe");
+
+      if (event.data?.call === "FromParent") {
+        iframe.contentWindow.postMessage(
+          {
+            call: "toParent",
+            value: user_data,
+          },
+          event.origin
+        );
+      }
+    };
+
+    window.addEventListener("message", (event) => {
+
+      if (event.origin !== "${f}") return;
+
+      post_layers(event);
+    });
+  <\/script>
+
+  <style>
+    #iframe {
+      display: block;
+      margin: 0 auto;
+      width: 100%;
+      height: 100vh;
+      transform: scale(${e});
+      -moz-transform: scale(${e});
+      -o-transform: scale(${e});
+      -webkit-transform: scale(${e});
+    }
+  </style>
+</head>
+
+<main>
+  <iframe
+          id="iframe"
+          src="${f}/profile/"
+          title="D3FEND Embedded" />
+</main>
+
+</html>
+`,bt=({domain:e,data:f})=>{const m=JSON.stringify(f),n=ut.crush(m),d=encodeURIComponent(n);return`${e}/profile/?profile=${d}`};function Ct(e){let f,m,n,d,g=`<h1 class="svelte-a38n6n"><span class="fancy-font">D3FEND</span> Embeddable Widget</h1> <h6 class="svelte-a38n6n">Use the D3FEND Embeddable Widget to embed an interactive D3FEND Profile in
+      another webpage.</h6>`,O,S,p,U,h,N,G,F,C,V,W,_,T,k=e[3].func({size:e[0].containerSize,domain:e[4],data:e[0]})+"",I,o,y,s,L=e[3].text+"",H,z,$,A,ee,fe,M,be,ce,Z,P,q,de,j,R,B,pe,Y;f=new rt({props:{title:"Embedded D3FEND Matrix",og_title:"MITRE Embedded D3FEND Layers",og_type:"website",og_url:"https://d3fend.mitre.org/",og_image:"https://d3fend.mitre.org/img/d3fend-og.png",og_description:"D3FEND Capability Layers Analysis."}}),p=new ht({});function te(l){e[13](l)}let Ce={inputClassName:"autocomplete-cursor-select",items:Object.values(e[5]),labelFieldName:"name",readonly:!0,onChange:e[12]};return e[3]!==void 0&&(Ce.selectedItem=e[3]),C=new We({props:Ce}),Te.push(()=>Ve(C,"selectedItem",te)),R=new mt({}),{c(){De(f.$$.fragment),m=E(),n=i("main"),d=i("section"),d.innerHTML=g,O=E(),S=i("code"),De(p.$$.fragment),U=E(),h=i("code"),N=i("h3"),G=ie(`Embed Code
+
+      `),F=i("span"),De(C.$$.fragment),W=E(),_=i("div"),T=i("pre"),I=ie(k),o=E(),y=i("div"),s=i("div"),H=ie(L),z=E(),$=i("button"),A=ie("Download"),fe=E(),M=i("button"),be=ie("Copy to Clipboard"),Z=E(),P=i("iframe"),de=E(),j=i("dialog"),De(R.$$.fragment),this.h()},l(l){Ie(f.$$.fragment,l),m=w(l),n=r(l,"MAIN",{style:!0,class:!0});var v=b(n);d=r(v,"SECTION",{id:!0,class:!0,"data-svelte-h":!0}),le(d)!=="svelte-1maub7"&&(d.innerHTML=g),O=w(v),S=r(v,"CODE",{id:!0,class:!0});var K=b(S);Ie(p.$$.fragment,K),K.forEach(c),U=w(v),h=r(v,"CODE",{id:!0,class:!0});var Q=b(h);N=r(Q,"H3",{class:!0});var ge=b(N);G=re(ge,`Embed Code
+
+      `),F=r(ge,"SPAN",{id:!0,class:!0});var Ee=b(F);Ie(C.$$.fragment,Ee),Ee.forEach(c),ge.forEach(c),W=w(Q),_=r(Q,"DIV",{id:!0,class:!0});var J=b(_);T=r(J,"PRE",{class:!0});var ne=b(T);I=re(ne,k),ne.forEach(c),J.forEach(c),o=w(Q),y=r(Q,"DIV",{id:!0,class:!0});var ae=b(y);s=r(ae,"DIV",{});var we=b(s);H=re(we,L),we.forEach(c),z=w(ae),$=r(ae,"BUTTON",{class:!0});var x=b($);A=re(x,"Download"),x.forEach(c),fe=w(ae),M=r(ae,"BUTTON",{class:!0});var Se=b(M);be=re(Se,"Copy to Clipboard"),Se.forEach(c),ae.forEach(c),Q.forEach(c),Z=w(v),P=r(v,"IFRAME",{id:!0,src:!0,title:!0,class:!0}),b(P).forEach(c),de=w(v),j=r(v,"DIALOG",{class:!0});var se=b(j);Ie(R.$$.fragment,se),se.forEach(c),v.forEach(c),this.h()},h(){a(d,"id","header"),a(d,"class","svelte-a38n6n"),a(S,"id","embed-settings"),a(S,"class","svelte-a38n6n"),a(F,"id","download-options"),a(F,"class","svelte-a38n6n"),a(N,"class","svelte-a38n6n"),a(T,"class","svelte-a38n6n"),a(_,"id","code-container"),a(_,"class","svelte-a38n6n"),a($,"class","download-code svelte-a38n6n"),$.disabled=ee=e[3].name==="Config Settings",a(M,"class","download-code svelte-a38n6n"),M.disabled=ce=e[3].name==="Config Settings",a(y,"id","download-container"),a(y,"class","svelte-a38n6n"),a(h,"id","embed-code"),a(h,"class","config-container svelte-a38n6n"),a(P,"id","layers-iframe"),lt(P.src,q="/profile/")||a(P,"src",q),a(P,"title","D3FEND Embedded"),a(P,"class","svelte-a38n6n"),a(j,"class","iframe-dialog"),Qe(n,"--container-size",e[0].containerSize),a(n,"class","svelte-a38n6n")},m(l,v){Ne(f,l,v),ve(l,m,v),ve(l,n,v),t(n,d),t(n,O),t(n,S),Ne(p,S,null),t(n,U),t(n,h),t(h,N),t(N,G),t(N,F),Ne(C,F,null),t(h,W),t(h,_),t(_,T),t(T,I),t(h,o),t(h,y),t(y,s),t(s,H),t(y,z),t(y,$),t($,A),t(y,fe),t(y,M),t(M,be),t(n,Z),t(n,P),e[16](P),t(n,de),t(n,j),Ne(R,j,null),e[17](j),B=!0,pe||(Y=[me(window,"message",e[6]),me($,"click",e[14]),me(M,"click",e[15]),me(j,"close",e[18])],pe=!0)},p(l,[v]){const K={};!V&&v&8&&(V=!0,K.selectedItem=l[3],Ue(()=>V=!1)),C.$set(K),(!B||v&25)&&k!==(k=l[3].func({size:l[0].containerSize,domain:l[4],data:l[0]})+"")&&qe(I,k),(!B||v&8)&&L!==(L=l[3].text+"")&&qe(H,L),(!B||v&8&&ee!==(ee=l[3].name==="Config Settings"))&&($.disabled=ee),(!B||v&8&&ce!==(ce=l[3].name==="Config Settings"))&&(M.disabled=ce),(!B||v&1)&&Qe(n,"--container-size",l[0].containerSize)},i(l){B||(Pe(f.$$.fragment,l),Pe(p.$$.fragment,l),Pe(C.$$.fragment,l),Pe(R.$$.fragment,l),B=!0)},o(l){ke(f.$$.fragment,l),ke(p.$$.fragment,l),ke(C.$$.fragment,l),ke(R.$$.fragment,l),B=!1},d(l){l&&(c(m),c(n)),$e(f,l),$e(p),$e(C),e[16](null),$e(R),e[17](null),pe=!1,et(Y)}}}function Et(e,f,m){let n,d,g,O;Je(e,oe,s=>m(0,g=s)),Je(e,it,s=>m(11,O=s));let S=null,p=null;const U={"Config Settings":{name:"Config Settings",text:"Profile Config Settings",func:()=>d},"index.html":{name:"index.html",download:"index.html",text:"Complete HTML/Javascript runnable script",func:vt},"Svelte Compenent":{name:"Svelte Compenent",download:"D3fendProfile.svelte",text:"Composable Svelte Compoenent runnable in Svelte Application",func:_t},"Profile data":{name:"Profile data",text:"Export Profile Data",func:()=>JSON.stringify(g,null,2).trim()},"Profile URL":{name:"Profile URL",text:"URL to D3FEND Custom Profile",func:bt}};let h=U["Config Settings"];ot(()=>{g.localStorageWarning&&p.showModal()}),ct(()=>{_e(oe,g.isReadonly=!1,g)});const N=s=>{var L;((L=s.data)==null?void 0:L.call)==="FromParent"&&S.contentWindow.postMessage({call:"toParent",value:g},s.origin)},G=s=>{s&&s.text&&m(3,h=s)},F=async(s,{func:L,download:H})=>{const z=L({size:g.containerSize,domain:n},g);ft(z,H)},C=async(s,{func:L})=>{const H=L({size:g.containerSize,domain:n},g);try{await navigator.clipboard.writeText(H)}catch(z){console.error("Failed to copy: ",z)}},V=s=>{s.close()},W=s=>G(s);function _(s){h=s,m(3,h)}const T=s=>F(s,h),k=s=>C(s,h);function I(s){Te[s?"unshift":"push"](()=>{S=s,m(1,S)})}function o(s){Te[s?"unshift":"push"](()=>{p=s,m(2,p)})}const y=()=>V(p);return e.$$.update=()=>{e.$$.dirty&2048&&m(4,n=O.url.origin),e.$$.dirty&1&&(d=JSON.stringify(g,["isReadonly","isDrawerOpen","showLegend","techniqueMappingMode","selectedProfile","matrixSize","containerSize"],2).trim())},[g,S,p,h,n,U,N,G,F,C,V,O,W,_,T,k,I,o,y]}class Lt extends tt{constructor(f){super(),nt(this,f,Et,Ct,Ze,{})}}export{Lt as component};
