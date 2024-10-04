9f9665794c442120084f63c2d62c9f6bfcf57d3e
Peter Kaloroumakis <pk@mitre.org>
2024-10-04 11:04:43 -0600

    automatic deployment to staging site

--- _app/env.js
+++ _app/env.js
@@ -1,1 +1,1 @@
-export const env={"PUBLIC_MODE":"build"}
+export const env={"PUBLIC_MODE":"build","PUBLIC_SPARQL_TARGET":"http://localhost:9899/blazegraph/namespace/d3fend/sparql","PUBLIC_SPARQL_INF_TARGET":"http://localhost:9899/blazegraph/namespace/d3fend_inf/sparql"}
---
+++ _app/immutable/entry/start.eOog8rw7.js
@@ -0,0 +1,1 @@
+import{c as a}from"../chunks/entry.CE2Gtjz_.js";export{a as start};
